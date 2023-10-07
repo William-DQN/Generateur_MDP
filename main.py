7c6c1fb6cbc5889e1adfed7eccb7249dafab1a21
William_DQN <william.duquennoy@outlook.be>
2023-10-07 17:04:56 +0200

    Création des listes

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #Le but de ce code est de fournir un mot de passe selon un nombre de caractères qu'il peut lui même définir.
 import string
 import random
+#Liste des minuscules
+list1 = [chr(i) for i in range(ord('a'), ord('z')+1)]
+#Liste des majuscules
+list2 = [chr(i) for i in range(ord('A'), ord('Z')+1)]
+#Liste des caractères spéciaux
+list3 = list(string.punctuation) + [chr(i) for i in range(ord('!'), ord('/')+1)]
