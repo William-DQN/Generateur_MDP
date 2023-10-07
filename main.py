452276095887a403a18fc3e839785757009e2575
William_DQN <william.duquennoy@outlook.be>
2023-10-07 18:27:22 +0200

    Mise en place de la boucle et des conditions d'utilisation

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ list3 = list(string.punctuation) + [chr(i) for i in range(ord('!'), ord('/')+1)]
 list4 = [str(i) for i in range(9)]
 #Mix de toutes les listes
 gigaliste = list1 + list2 + list3 + list4
+#On va demander au moins 8 caractères et max 116 caractères afin d'avoir un minimum de sécurité et éviter les bugs 
+def gener(nbruser):
+    if nbruser < 8 or nbruser > 116:
+        return None
+    else:
+        password = ''.join(random.choice(gigaliste) for _ in range(nbruser))
+        return password
+
+nbruser = None
+#On boucle jusqu'à ce que l'utilisateur respecte les conditions imposées.
+while True:
+    try:
+        nbruser = int(input("Veuillez entrer un nombre entre 8 et 116 : "))
+        password = gener(nbruser)
+        if password:
+            print("Votre nouveau mot de passe est :"+ password)
+            break
+        else:
+            print("Vous n'avez pas respecté la condition. Il est important d'avoir un mot de passe d'une longueur minimale de 8.")
+    except ValueError:
+        print("Veuillez entrer un nombre entier.")
